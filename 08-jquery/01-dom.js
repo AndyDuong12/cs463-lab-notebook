59a42ce2354d4071f16266f1d9af1df9e0836a12
Andy <andyduong03@gmail.com>
2024-11-20 15:21:05 -0800

    01-dom.js completed

--- 08-jquery/01-dom.js
+++ 08-jquery/01-dom.js
@@ -3,7 +3,7 @@
 
 // Option #1
 // Fetch JavaScript objects representing specific elements in the DOM
-let userInput1 = document.querySelector('#userInput1');
+/*let userInput1 = document.querySelector('#userInput1');
 let copy = document.querySelector('#copy');
 let output1 = document.querySelector('#output1');
 
@@ -14,7 +14,14 @@ copy.addEventListener('click', handleClick);
 function handleClick(event) {
   console.log('click event', event);
   output1.textContent = userInput1.value;
-}
+}*/
+
+$("document").ready(() => {
+  $("#copy").on("click", (event) => {
+    console.log("click event", event);
+    $("#output1").text($("#userInput1").val());
+  });
+});
 
 // Option #2
 // document.getElementById('copy').onclick = () => {
@@ -35,7 +42,7 @@ function handleClick(event) {
 // When the user enters input text, copy the user input to the output area
 
 // Fetch JavaScript objects representing specific elements in the DOM
-let userInput2 = document.querySelector('#userInput2');
+/*let userInput2 = document.querySelector('#userInput2');
 let output2 = document.querySelector('#output2');
 
 // Add an event listener on the target element
@@ -45,4 +52,10 @@ userInput2.addEventListener('input', handleInput);
 function handleInput(event) {
   console.log('click event', event);
   output2.textContent = userInput2.value;
-}
+}*/
+
+$("document").ready(() => {
+  $("#userInput2").on("input", () => {
+    $("#output2").text($("#userInput2").val());
+  });
+});
